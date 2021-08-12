c6a85472554a9b53d4240f6d023996da0a55b2ba
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-08-12 18:54:26 +1200

    create expenseitem component

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+// Custom Components
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-      <h1> Hello World! </h1>
-      <p>Something else</p>
+      <h1>Hello World!</h1>
+      <ExpenseItem />
     </div>
   );
 }
